96d6d821d533291fd345da8862117e902cc87254
Prato dewan <pratodewan1@gmail.com>
2023-09-09 08:50:16 +0600

    first commit
    initialized project with required dependencies

---
+++ src/test/java/com/prato/onlinebooklibrary/OnlineBookLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prato.onlinebooklibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBookLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
